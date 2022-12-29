757150fcd078a87672a8f3163956be49209ad1d5
seokhoyu <mydreams1228@naver.com>
2022-12-29 16:45:55 +0900

    [빌드환경 셋팅]Cmake 추가

---
+++ CMakeFiles/diag_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diag_server.dir/diag_app.c.o"
+  "diag_server"
+  "diag_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/diag_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

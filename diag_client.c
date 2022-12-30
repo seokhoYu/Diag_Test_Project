712583687303dfef977edee66966ee25f18fc5c4
유석호 <mydreams1228@gmail.com>
2022-12-30 23:05:43 +0900

    [수정] Code update

--- diag_client.c
+++ diag_client.c
@@ -9,9 +9,16 @@
 #include <pthread.h>
 
 #define DEBUG 0
+#define input_lenth 2
+#define max_lenth 1400
+
+uint8_t Physical_Req_ECU[0] = 0x05;
+uint8_t Physical_Res_ECU[0] = 0xB1;
+
 
 void Start_Menu(){
     printf("------- Diag Message Start -------\n");
+    printf("0. press 'q' Ehternet Connect\n");
     printf("1. Press 's' Session Control Message\n");
     printf("2. Press 'a' Security Access Message\n");
     printf("3. Press 'k' SendKey Message\n");
@@ -72,15 +79,30 @@ int Create_SOCK_Client (char* ip_adrress, uint32_t port, struct sockaddr_in* ser
     printf("Client SOCKET Create Fail\n");
     }
 
-    memset(serverAddr,0,sizeof(serverAddr));
+    memset(serverAddr,0,sizeof(*serverAddr));
     serverAddr->sin_family = AF_INET;
     serverAddr->sin_addr.s_addr = inet_addr(ip_adrress);   
     serverAddr->sin_port=htons(port);
     return clnt_sock;
 }
 
+void Send_Session_Control(int sock){
+    uint8_t message[max_lenth];
+    message[0] = 0xCA;
+    message[1] = 0xFC;
+    message[2] = 0xBC;
+    message[3] = 0x00;
+    message[4] = 0x00;
+    message[5] = 0x00;
+    message[6] = 0x0A;
+    
+
+    write(sock,message,4);
+
+}
+
 int main(int argc, char* argv[]){
-   
+    char Key_input[input_lenth];
     if(argc!=3){
         printf("Usage : %s <IP> <PORT>\n",argv[0]);
         exit(1);
@@ -91,7 +113,7 @@ int main(int argc, char* argv[]){
     struct sockaddr_in serverAddr;
     int sockaddr_len;
     int clnt_sock = Create_SOCK_Client(argv[1],serv_port,&serverAddr);
-   
+    int ret;
     sockaddr_len = sizeof(serverAddr);
     if(clnt_sock==-1){
         printf("Create_SOCK_Client() fail\n");
@@ -100,14 +122,40 @@ int main(int argc, char* argv[]){
         printf("Create Sock Success\n");
     }
 
-    if(connect(clnt_sock,(struct sockaddr*)&serverAddr,sizeof(serverAddr))==-1){
-        printf("Connect() Fail\n");
-    }
-    else{
-        printf("Connected....\n");
-    }
+    // if(connect(clnt_sock,(struct sockaddr*)&serverAddr,sizeof(serverAddr))==-1){
+    //     printf("Connect() Fail\n");
+    // }
+    // else{
+    //     printf("Connected Server....\n");
+    // }
 
     while(1){
+    
+    Start_Menu();
+    fputs("Input message on Key Board!! \n",stdout);
+    fgets(Key_input, input_lenth, stdin);
+
+    switch(Key_input[0]){
+        case 'q':
+        ret = connect(clnt_sock,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+        if(ret!=-1){
+            printf("Cnnected Sever....\n");
+        }
+        else{
+            printf("Connect() Fail\n");
+        }
+        break;
+
+        case 's':
+        printf("Session Control Start!!\n");
+        Send_Session_Control(clnt_sock);
+        break;
+
+        case 'c':
+        printf("Disconnected Ehtnernet Connection!\n");
+        close(clnt_sock);
+    }
+    memset(Key_input,0,sizeof(Key_input));
 
     }
 
